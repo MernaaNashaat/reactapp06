cc46c13d76b0c0dacd8b29111313a3602755972a
MernaaNashaat <nashaatmerna54@gmail.com>
2025-06-13 20:43:06 +0300

    add

--- src/ui-components/IPhone13141.jsx
+++ src/ui-components/IPhone13141.jsx
@@ -3426,7 +3426,7 @@ export default function IPhone13141(props) {
     src="https://bucketgp.s3.eu-north-1.amazonaws.com/pics_for_GP/Screenshot_2025-06-13_202605-removebg-preview.png"
      alt="Background image"
     style={{
-      width: '93px',
+      width: '92px',
       height: 'auto',
       position: 'absolute',
       top: '147px',
